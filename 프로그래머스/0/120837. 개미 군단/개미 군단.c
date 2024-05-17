d4e49049edf1b4b4a8fbc0eb58df3ca5c27c651d
leedh0297 <107226756+leedh0297@users.noreply.github.com>
2024-05-17 15:30:56 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int hp) {
+    int answer = 0;
+    answer=hp/5;
+    answer=answer+(hp%5)/3;
+    answer=answer+((hp%5)%3)/1;
+    return answer;
+}
